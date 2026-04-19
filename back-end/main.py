86f28d4c4986538f059fa2345d67f8d3c796da3f
Rafael <132296553+R4f4ell@users.noreply.github.com>
2026-04-18 21:39:30 -0300

    feat: preparando config para deploy

--- back-end/main.py
+++ back-end/main.py
@@ -12,10 +12,10 @@ def health_check():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(countries_router)
-
